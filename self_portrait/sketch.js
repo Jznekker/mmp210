df7ab188681b4c62d24e24c94c625719e38bfcda
Jznekker <57229353+Jznekker@users.noreply.github.com>
2020-02-06 17:38:04 -0500

    face

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -5,11 +5,41 @@ function setup() {
 }
 
 function draw() {
-	background('blue');
+	background('white');
 	//Face
+	fill('lightblue');
 	ellipse(320, 180, 300);
+	
 	//Right eye
-	ellipse(400, 160, 20);
+	fill('white');
+	ellipse(400, 160, 50);
+	//right iris
+	fill('black');
+	ellipse(410, 170, 20);
+
 	//Left eye
-	ellipse(350, 160, 30);
+	fill('white');
+	ellipse(240, 160, 50);
+	//left iris
+	fill('black');
+	ellipse(250, 170, 20)
+	//mouth
+	fill('black');
+	ellipse(320, 230, 50, 80);
+
+	//tongue
+	fill('red');
+	ellipse(320, 250, 40, 40);
+
+	//Icecream
+	fill(247, 219, 236);
+	ellipse(550, 250, 30, 30);
+	fill(248, 200, 226);
+	ellipse(520, 250, 30, 30);
+	fill(255, 253, 208);
+	ellipse(535, 225, 30, 30);
+
+	//cup
+	fill('white');
+	rect(495, 260, 80, 40, 20);
 }
